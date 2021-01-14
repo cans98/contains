a0046e772b3f7afe4f16efe1022ba8bc281d06b5
cans98 <53710530+cans98@users.noreply.github.com>
2021-01-14 21:42:35 +0300

    Add files via upload

---
+++ contains_method/contains_method/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iterative_game
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string sifre = "cancan9898..";
+            Console.WriteLine("sifre giriniz");
+
+
+
+            while (true) // kullanıcı doğru şifreyi yazana kadar tekrarlar
+            {
+                string girilenSifre = Convert.ToString(Console.ReadLine());
+                bool b = sifre.Contains(girilenSifre);
+
+
+                if (b == true)
+                {
+                    Console.WriteLine("sifre doğru");
+                    break;
+                }
+                else
+                {
+
+                    Console.WriteLine("lütfen şifre giriniz");
+                    string tekrar = Convert.ToString(Console.ReadLine());
+                    girilenSifre = tekrar;
+
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
